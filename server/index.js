d7293c594980e6138287131a9ea82286ce0fa12b
SSakshi-75 <sakshisingh092003@gmail.com>
2026-07-30 00:14:23 +0530

    os and cluster remove

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-
 import express from "express";
 import { config } from "dotenv";
 import compression from "compression";
@@ -6,51 +5,32 @@ import cookieParser from "cookie-parser";
 import Db from "./src/database/Db.js";
 import cors from "cors";
 import morgan from "morgan";
-import cluster from "cluster";
-import os from "os";
 import contactRoutes from "./src/routes/contact.routes.js";
 import adminRoutes from "./src/admin/admin.route.js";
 
 config();
 
 const PORT = process.env.PORT || 5000;
-const numCPUs = os.cpus().length;
 
-if (cluster.isPrimary) {
-  console.log("Master process running:", process.pid);
+const app = express();
 
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:5173", credentials: true }));
 
-  cluster.on("exit", (worker) => {
-    console.log("Worker died:", worker.process.pid);
-    cluster.fork();
-  });
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan("dev"));
 
-} else {
+app.get("/", (req, res) => {
+  res.send("Welcome to Auto Generated Backend!");
+});
 
-  const app = express();
+app.use("/api/contact", contactRoutes);
+app.use("/api/admin", adminRoutes);
 
-  app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:5173", credentials: true }));
-
-  app.use(compression());
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(cookieParser());
-  app.use(morgan("dev"));
-
-  app.get("/", (req, res) => {
-    res.send("Welcome to Auto Generated Backend!");
-  });
-
-  app.use("/api/contact", contactRoutes);
-  app.use("/api/admin", adminRoutes);
-
-  Db().then(() => {
-    app.listen(PORT, () =>
-      console.log("🚀 Server running at http://localhost:" + PORT)
-    );
-  });
-
-}
+Db().then(() => {
+  app.listen(PORT, () =>
+    console.log("🚀 Server running at http://localhost:" + PORT)
+  );
+});
