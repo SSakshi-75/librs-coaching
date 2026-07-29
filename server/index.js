a9db0018947bf7d325120e9dbb2b6f858c4e26ea
SSakshi-75 <sakshisingh092003@gmail.com>
2026-07-29 17:14:14 +0530

    connect frontend with backend

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ if (cluster.isPrimary) {
 
   const app = express();
 
-  app.use(cors({ origin: "*", credentials: true }));
+  app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:5173", credentials: true }));
 
   app.use(compression());
   app.use(express.json());
